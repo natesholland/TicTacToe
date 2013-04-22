db53db3dd3b50e33b22beca09cd183b9e1d36c2a
NateSHolland <natesholland@gmail.com>
2013-04-22 15:51:48 -0700

    building the skeleton

--- Board.java
+++ Board.java
@@ -2,8 +2,14 @@ package TicTacToe;
 
 public class Board {
 
+	private Peice[][] board;
+
     public Board() {
-    
+    board = new Peice[3][3];
+    }
+
+    public void makeMove(Move m) {
+
     }
 
 }
---
+++ Move.java
@@ -0,0 +1,20 @@
+package TicTacToe;
+
+public class Move {
+	
+	private int col;
+
+	private int row;
+
+	private Peice p;
+
+
+	public Move(int col, int row, Peice p) {
+		col = col;
+		row = row;
+		p = p;
+	}
+
+
+
+}
